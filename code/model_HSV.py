cb527e3f6f36e1d8abb46d33e3a76f6738835f31
Leshanshui YANG <44177036+TilkeyYANG@users.noreply.github.com>
2019-12-11 21:26:05 +0100

    Add files via upload

---
+++ code/model_HSV.py
@@ -0,0 +1,25 @@
+import keras
+
+from keras.layers import Conv2D, MaxPooling2D, Input, Dense, Flatten
+from keras.models import Model
+
+from keras.optimizers import Adam
+
+def model(input_size,output_size):
+    print(input_size)
+    inputs = Input(shape=input_size)
+    model = Conv2D(11, 11, activation="relu", padding="same")(inputs)
+    model = MaxPooling2D(pool_size=(2,2)) (model)
+    model = Conv2D(9,9,activation="relu",padding='same') (model)
+    model = MaxPooling2D(pool_size=(2,2)) (model)
+    model = Conv2D(7,7,activation="relu",padding="same") (model)
+    model = MaxPooling2D(pool_size=(2,2)) (model)
+
+    model = Flatten()(model)
+    output = Dense(output_size[1],activation="softmax") (model)
+
+    model = Model(input=inputs,outputs = output)
+    model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+
